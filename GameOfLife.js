f3a09f333ac47418f430792b06b3d4d3f46b2f2f
Aidan Farhi <aidanfarhi@Aidans-MacBook-Air.local>
2020-07-24 09:03:03 -0400

    count neighbor function working with 3*3 grid

--- GameOfLife.js
+++ GameOfLife.js
@@ -50,73 +50,172 @@ class GameOfLife {
    * Return the amount of living neighbors around a given coordinate.
    */
 
-  livingNeighbors(row, col) {
-    // TODO: Return the count of living neighbors.
-    // make a count of neighbors and get an array of
-    // the position of the neighbor
+  livingNeighbors(r, c) {
+    let count = 0
+    let arr = this.board
+    console.log('board coming in:', arr)
+    console.log('typeof arr[r - 1]', typeof arr[r - 1])
+    console.log('typeof arr[c - 1]', typeof arr[c - 1])
+    console.log('arr[r]', arr[r])
+    console.log('arr[r][c]', arr[r][c])
+    console.log('arr[r][c + 1]', arr[r][c + 1])
 
-    // arr[i][i] // self
-    // arr[i][i + 1] // right
-    // arr[i][i - 1] // left
-    // arr[i - 1][i] // top
-    // arr[i - 1][i - 1] // top left
-    // arr[i - 1][i + 1] // top right
-    // arr[i + 1][i] // bottom
-    // arr[i + 1][i + 1] // bottom right
-    // arr[i + 1][i - 1] // bottom left
-
-    let neighbors = {
-      right: {
-        pos: [row, col + 1],
-        val: this.board[row][col + 1]
-      },
-      left: {
-        pos: [row, col - 1],
-        val: this.board[row][col - 1]
-      },
-      top: {
-        pos: [row - 1, col],
-        val: this.board[row - 1][col]
-      },
-      bottom: {
-        pos: [row + 1, col],
-        val: this.board[row + 1][col]
-      },
-      topRight: {
-        pos: [row - 1, col + 1],
-        val: this.board[row - 1][col + 1]
-      },
-      bottomRight: {
-        pos: [row + 1, col + 1],
-        val: this.board[row + 1][col + 1]
-      },
-      topLeft: {
-        pos: [row - 1, col - 1],
-        val: this.board[row - 1][col - 1]
-      },
-      bottomLeft: {
-        pos: [row + 1, col - 1],
-        val: this.board[row + 1][col - 1]
+    if (!arr[r - 1] && !arr[c - 1]) {
+      if (arr[r][c + 1] === 1) {
+        console.log('hit')
+        count++
       }
+      if (arr[r + 1][c] === 1) {
+        count++
+      }
+      if (arr[r + 1][c + 1] === 1) {
+        count++
+      }
+      return count
+    }
+    // upper right
+    if (!arr[r - 1] && !arr[c + 1]) {
+      if (arr[r][c - 1] === 1) {
+        count ++
+      }
+      if (arr[r + 1][c] === 1) {
+        count++
+      }
+      if (arr[r + 1][c - 1] === 1) {
+        count++
+      }
+      return count
+    }
+    // middle top
+    if (!arr[r - 1]) {
+      if (arr[r][c - 1] === 1) {
+        count++
+      }
+      if (arr[r][c + 1] === 1) {
+        count++
+      }
+      if (arr[r + 1][c - 1] === 1) {
+        count++
+      }
+      if (arr[r + 1][c] === 1) {
+        count++
+      }
+      if (arr[r + 1][c + 1] === 1) {
+        count++
+      }
+      return count
+    }
+    // bottom left
+    if (!arr[r + 1] && !arr[c - 1]) {
+      if (arr[r][c + 1] === 1) {
+        count++
+      }
+      if (arr[r - 1][c] === 1) {
+        count++
+      }
+      if (arr[r - 1][c + 1] === 1) {
+        count++
+      }
+      return count
+    }
+    // bottom right
+    if (!arr[r + 1] && !arr[c + 1]) {
+      if (arr[r][c - 1] === 1) {
+        count++
+      }
+      if (arr[r - 1][c] === 1) {
+        count++
+      }
+      if (arr[r - 1][c - 1] === 1) {
+        count++
+      }
+      return count
+    }
+    // bottom middle
+    if (!arr[r + 1]) {
+      if (arr[r][c - 1] === 1) {
+        count++
+      }
+      if (arr[r][c + 1] === 1) {
+        count++
+      }
+      if (arr[r - 1][c - 1] === 1) {
+        count++
+      }
+      if (arr[r - 1][c] === 1) {
+        count++
+      }
+      if (arr[r - 1][c + 1] === 1) {
+        count++
+      }
+      return count
+    }
+    // middle left
+    if (!arr[c - 1]) {
+      if (arr[r][c + 1] === 1) {
+        count++
+      }
+      if (arr[r - 1][c] === 1) {
+        count++
+      }
+      if (arr[r - 1][c + 1] === 1) {
+        count++
+      }
+      if (arr[r + 1][c] === 1) {
+        count++
+      }
+      if (arr[r + 1][c + 1] === 1) {
+        count++
+      }
+      return count
+    }
+    // middle right
+    if (!arr[c + 1]) {
+      if (arr[r][c - 1] === 1) {
+        count++
+      }
+      if (arr[r - 1][c - 1] === 1) {
+        count++
+      }
+      if (arr[r - 1][c] === 1) {
+        count++
+      }
+      if (arr[r + 1][c - 1] === 1) {
+        count++
+      }
+      if (arr[r + 1][c] === 1) {
+        count++
+      }
+      return count
     }
 
-    //  now we make an array of our valid neighbors
-    //  this will be an array of neighbor objects and their respective positions
-    //  and values
-
-    let alive = 0
-    for (let posish in neighbors) {
-      let position = neighbors[posish]
-      for (let item in position) {
-        // this is an array
-        let state = item.value  
-        if (state === 1) {
-          alive++
-        }
-      }
+    if (arr[r][c - 1] === 1) {
+      count++
     }
-    // this should give the count of alive neighbors
-    return alive
+    if (arr[r][c + 1] === 1) {
+      count++
+    }
+    if (arr[r - 1][c - 1] === 1) {
+      count++
+    }
+    if (arr[r - 1][c] === 1) {
+      count++
+    }
+    if (arr[r - 1][c + 1] === 1) {
+      count++
+    }
+    if (arr[r + 1][c - 1] === 1) {
+      count++
+    }
+    if (arr[r + 1][c] === 1) {
+      count++
+    }
+    if (arr[r + 1][c + 1] === 1) {
+      count++
+    }
+
+    return count
+
   }
 
   /**
@@ -129,22 +228,25 @@ class GameOfLife {
     // on the existing board and determine, based on it's neighbors,
     // whether the cell should be dead or alive in the new board
     // (the next iteration of the game)
+    console.log('line 194 newBoard:', newBoard)
     let board = this.board
     for (let k = 0; k < board.length; k++) {
       let row = board[k]
-      for (let l = 0; l = row.length; l++) {
+      for (let l = 0; l < row.length; l++) {
         let col = row[l]
-        let neighborCount = livingNeighbors(row, col)
-        if (neighborCount > 3) {
-          newBoard[row][col] = 0
+        let neighborCount = this.livingNeighbors(k, l)
+        console.log('this is the neighbor count, line 238:', neighborCount)
+        if (neighborCount > 3 && board[row][col] === 1) {
+          newBoard[k][l] = 0
         } else if (neighborCount < 2) {
-          newBoard[row][col] = 0
+          newBoard[k][l] = 0
         } else if (neighborCount > 1 && neighborCount < 4) {
-          newBoard[row][col] = 1
+          newBoard[k][l] = 1
+        } else if (neighborCount > 3 && board[row][col] === 0) {
+          newBoard[k][l] = 1
         }
       }
     }
-    //
     // You need to:
     // 1. Count alive neighbors for all cells
     // 2. Set the next state of all cells in newBoard,
